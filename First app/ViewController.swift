71b47890de22848add9fccb7bc8558e2d8ad7713
Lyndon Black <lyndon.black@gmail.com>
2018-06-23 12:17:19 +0100

    Out first Commit to Git

--- First app/ViewController.swift
+++ First app/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    @IBOutlet weak var MyLabel: UILabel!
+    
+    @IBAction func Button1(_ sender: Any) {
+        
+        count = count + 1
+        
+        print(count)
+        
+        if count >= 5 {
+            
+        view.backgroundColor = UIColor.red
+        
+        MyLabel.text = "I am God"
+        
+            if count >= 10 {
+                
+                count = 0
+                
+                view.backgroundColor = UIColor.cyan
+                
+                MyLabel.text = "Who are you"
+                
+            }
+            
+    }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.cyan
+        
+        
+        MyLabel.text = "Living Large"
     }
 
     override func didReceiveMemoryWarning() {
